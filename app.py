8ba9e3fe12b0a49addf02623af24d36b346f2397
meto820 <metekurubal12@gmail.com>
2025-09-20 17:27:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -204,3 +204,6 @@ def delete_user():
 if __name__ == "__main__":
     print("Flask uygulaması başlatılıyor...")
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port
+
